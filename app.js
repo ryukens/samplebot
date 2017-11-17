bcf9876335a0535a40d00ee6ef5d47719a83f366
ryukens <33704833+ryukens@users.noreply.github.com>
2017-11-17 18:17:48 -0500

    added gallo

--- app.js
+++ app.js
@@ -347,7 +347,7 @@ request.on('response', function(response) {
 						checkAccount(senderID, "perro");
 					break;
 					case 'account.movement':
-						checkAccount(senderID, "movimiento");
+						checkAccount(senderID, "gallo");
 					break;
 					default:
 					console.log('unknown action...');
