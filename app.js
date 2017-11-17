f4654ecba835acb1cf8b35b8821cebc20f42454f
ryukens <33704833+ryukens@users.noreply.github.com>
2017-11-17 18:21:33 -0500

    funnycat

--- app.js
+++ app.js
@@ -347,7 +347,7 @@ request.on('response', function(response) {
 						checkAccount(senderID, "perro");
 					break;
 					case 'account.movement':
-						checkAccount(senderID, "cat and roaster");
+						checkAccount(senderID, "funnycat");
 					break;
 					default:
 					console.log('unknown action...');
